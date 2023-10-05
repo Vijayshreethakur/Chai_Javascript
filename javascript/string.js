1e616dd54502c1a7786c2223e505193035b2ad3a
Vijayshree Thakur <143580947+Vijayshreethakur@users.noreply.github.com>
2023-10-05 08:10:02 +0000

    main

---
+++ javascript/string.js
@@ -0,0 +1,24 @@
+let myName = "vijayshree"
+let study = "JavaScript"
+
+// console.log(`my name is ${myName} and currently i am studing ${study}.`)
+
+// console.log(myName.length)
+// console.log(myName.charAt(4))
+// console.log(myName.indexOf("y"));
+// console.log(myName.substring(0,4))
+// console.log(myName.toUpperCase());
+// console.log(myName.toLocaleUpperCase());
+
+// const myorjname = "           vijayshreee             "
+// console.log(myorjname.trim());
+// console.log(myorjname);
+
+let address = "mumbai-thane-panvel-rajgah"
+// console.log(address.replace("mumbai", "bombay"));
+// console.log(address);
+
+// console.log(address.includes("tara"))
+
+console.log(address.split(" - "));
+console.log(address.small())
