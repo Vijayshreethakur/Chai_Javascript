475d2eef2594d0f5617719f14c42a27598aa3d77
Vijayshree Thakur <143580947+Vijayshreethakur@users.noreply.github.com>
2023-10-05 10:15:44 +0000

    change

---
+++ 01_basics/date_time.js
@@ -0,0 +1,27 @@
+let myDate = new Date();
+// console.log(myDate);
+
+// console.log(myDate.toString()) //Thu Oct 05 2023 09:44:07 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.toDateString()); //Thu Oct 05 2023 09:46:29 GMT+0000 (Coordinated Universal Time)
+// Thu Oct 05 2023
+
+// console.log(myDate.toISOString()) //2023-10-05T09:48:04.106Z
+
+// console.log(myDate.toJSON()) //2023-10-05T09:48:44.448Z
+// 2023-10-05T09:48:44.448Z
+
+// console.log(myDate.toLocaleDateString()) //10/5/2023
+
+// console.log(myDate.getDate())
+
+
+// costomise Date
+
+let myCreatedDate = new Date(2023,0,23);
+console.log(myCreatedDate.toDateString()); //Mon Jan 23 2023
+
+console.log(myCreatedDate.getTime());
+
+
+
---
+++ 01_basics/number_maths.js
@@ -0,0 +1,25 @@
+let num = 400
+// console.log(num)
+
+let balance = new Number(2000000)
+// console.log(balance); //[ Number:200]
+
+
+// console.log(balance.toString()) //100
+// console.log(typeof balance.toString())
+
+// console.log(balance.toFixed(2)) //100.00
+
+// console.log(balance.toPrecision(4)); //200.0
+
+// console.log(balance.toLocaleString());//2,000,000
+// console.log(balance.toLocaleString("en-IN"))//20,00,000 in india currency
+
+// *****************************Maths*********************************
+
+// console.log(Math.abs(-11)); //11
+// console.log(Math.round(10.9)) //11
+// console.log(Math.ceil(11.2)); //12
+// console.log(Math.floor(11.3)); //11
+// console.log(Math.random())
+// console.log(Math.floor(Math.random()*10)+1);
