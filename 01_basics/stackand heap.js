2b61adf7d9292a457943cb5e425ed0c194239291
Vijayshree Thakur <143580947+Vijayshreethakur@users.noreply.github.com>
2023-10-17 08:34:31 +0000

    changw

---
+++ 01_basics/stackand heap.js
@@ -0,0 +1,26 @@
+/*
+There are 2 types of datatype
+1. Primitive datatype:-
+    1.String
+    2. Number
+    3. Boolean (true / false)
+    4. null (iska matlab hai empty or zero nahi hai)
+    5. undefined (aapne variable declare kardiya hai but usmein aap kya value dalenge aapne decide nahi kiya hai)
+    6. Symbol (kisi bhi value ko unique banane ke liye use kiya jata hai)
+    7. BigInt (jo badi values hoti hai oo hum BigInt mein use karte hai)
+
+2. Preference datatype (Non Primitive datatype)
+    1. Array ( [ ] )
+    2. Object ( { } )
+    3. Function ( function( ){ } )
+
+
+
+    Primitive dataType goes in stack memory; And it provides copy of that element.
+So when we change something (through assigned variable) it gets changed in copy of element  not in original form.
+
+
+Opposite of this. Non primitive dataType gets stored in HEAP memory. 
+And it gives reference value to assigned elements. 
+So when assigned variable makes changes it changes the both values(it referce the same )
+*/
